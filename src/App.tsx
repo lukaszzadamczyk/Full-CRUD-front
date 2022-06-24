20b8a6d740c506b2ef985cb2f9ec7f8beb812556
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-24 10:40:44 +0100

    Created and config common types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Test } from "types";
 
 function App() {
+  const foobar: Test = {
+    name: "Lukasz",
+  };
+
   return (
     <div className="App">
       <header className="App-header">
