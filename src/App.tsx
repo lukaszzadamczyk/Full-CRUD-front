28c1183ea59bc07d835e25ba08d88f4e721b7ca2
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-24 21:18:15 +0100

    Added AddUser component and styles.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { Home } from "../src/components/pages/Home/Home";
+import { AddUser } from "../src/components/pages/AddUser/AddUser";
 
 import "react-toastify/dist/ReactToastify.css";
 
@@ -11,6 +12,7 @@ function App() {
       <ToastContainer position="top-center" />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/addUser" element={<AddUser />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/pages/AddUser/AddUser.tsx
@@ -0,0 +1,89 @@
+import React, { ChangeEvent, FormEvent, useEffect, useState } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
+import { Btn } from "../../common/Btn/Btn";
+import { UserEntity } from "types";
+
+import "./AddUser.css";
+import { toast } from "react-toastify";
+import axios from "axios";
+import { Input } from "../../common/Input/Input";
+import { Label } from "../../common/Label/Label";
+
+const initialState: UserEntity = {
+  name: "",
+  contact: "",
+  email: "",
+};
+
+export const AddUser = () => {
+  const [newDataUser, setNewDataUser] = useState<UserEntity>(initialState);
+
+  const { name, contact, email } = newDataUser;
+
+  const navigate = useNavigate();
+
+  const handleFormSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    if (!name || !contact || !email) {
+      toast.error("Please provide correct value.");
+    } else {
+      axios
+        .post("http://localhost:3001/api/add", {
+          name,
+          contact,
+          email,
+        })
+        .then(() => {
+          setNewDataUser({ name: "", contact: "", email: "" });
+        })
+        .catch((err) => toast.error(err.response.data));
+      toast.success(`User ${name} Added 😎`);
+      setTimeout(() => {
+        navigate(-1);
+      }, 500);
+    }
+  };
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setNewDataUser({ ...newDataUser, [name]: value });
+  };
+
+  return (
+    <div className="form_container">
+      <form className="add_form" onSubmit={handleFormSubmit}>
+        <Label htmlFor="name" text="Name" />
+        <Input
+          id="name"
+          type="text"
+          value={name}
+          name="name"
+          placeholder="Your Name"
+          change={handleInputChange}
+        />
+        <Label htmlFor="contact" text="Contact" />
+        <Input
+          id="contact"
+          type="number"
+          name="contact"
+          placeholder="Your Phone Number..."
+          value={contact}
+          change={handleInputChange}
+        />
+        <Label htmlFor="email" text="Email" />
+        <Input
+          id="email"
+          type="email"
+          name="email"
+          placeholder="Your Email..."
+          value={email}
+          change={handleInputChange}
+        />
+        <Btn classNameBtn="btn btn-save" text="save" />
+        <Link to="/">
+          <Btn classNameBtn="btn btn-home" text="Go Home" />
+        </Link>
+      </form>
+    </div>
+  );
+};
