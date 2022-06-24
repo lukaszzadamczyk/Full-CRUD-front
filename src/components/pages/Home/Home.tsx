eaf2a6e9621f626ac0c0b620c5adcf17649cffd8
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-24 15:45:32 +0100

    Added Home component and Btn component

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
+import { toast } from "react-toastify";
+import { UserEntity } from "types";
+import { Btn } from "../../common/Btn/Btn";
+
+import "./Home.css";
+
+export const Home = () => {
+  const [data, setData] = useState<UserEntity[]>([]);
+
+  const loadData = async () => {
+    const response = await axios.get("http://localhost:3001/api/get");
+    setData(response.data);
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  return (
+    <div className="users_container">
+      <table className="users_table">
+        <thead className="users_table_header">
+          <tr>
+            <th>No.</th>
+            <th>Name</th>
+            <th>Contact</th>
+            <th>Email</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody className="users_table_body">
+          {data.map((item, index) => {
+            return (
+              <tr key={item.id}>
+                <th scope="row">{index + 1}</th>
+                <td>{item.name}</td>
+                <td>{item.contact}</td>
+                <td>{item.email}</td>
+                <td className="users_buttons">
+                  <Link to={`/update/${item.id}`}>
+                    <Btn classNameBtn="btn btn-edit" text="Edit" />
+                  </Link>
+                  <Btn classNameBtn="btn btn-delete" text="Delete" />
+                  <Link to={`/view/${item.id}`}>
+                    <Btn classNameBtn="btn btn-view" text="View" />
+                  </Link>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
