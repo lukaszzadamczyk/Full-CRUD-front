7b3975b4b0d2f6e095506e05afb0beeccbc124e1
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-24 14:34:28 +0100

    Added router to app.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,21 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { Home } from "../src/components/pages/Home/Home";
 
-import logo from "./logo.svg";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
-import { Test } from "types";
-
 
 function App() {
-  const foobar: Test = {
-    name: "Lukasz",
-  };
-
   return (
-    <div className="App">
-      <h1>Hello</h1>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <ToastContainer position="top-center" />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
