3aee869764e98106c38938ff232ea4d8557d2488
Lukasz Adamczyk <lukaszz.adamczyk@gmail.com>
2022-06-29 19:35:05 +0100

    Deleted input css.

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import "./App.css";
 function App() {
   return (
     <BrowserRouter>
-      <Header></Header>
+      <Header />
       <ToastContainer position="top-center" className="toast" />
       <Routes>
         <Route path="/" element={<Home />} />
